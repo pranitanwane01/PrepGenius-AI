245e01317ca535cef5c7b6632d8255cc31264c2c
PRANIT ANWANE <pranitanwane@PRANITs-MacBook-Air.local>
2026-04-09 00:47:06 -0700

    fix cors final

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,4 @@
-// require("dotenv").config()
-// const app = require("./src/app")
-// const connectToDB = require("./src/config/database")
 
-// connectToDB()
-
-
-// app.listen(3000, () => {
-//     console.log("Server is running on port 3000")
-// })
 
 require("dotenv").config()
 
--- Backend/src/app.js
+++ Backend/src/app.js
@@ -17,8 +17,23 @@ app.use(cors({
   credentials: true
 }))
 
+app.use(express.json())
+app.use(cookieParser())
+
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://prep-genius-ai-kohl.vercel.app"
+  ],
+  credentials: true
+}))
+
 app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://prep-genius-ai-kohl.vercel.app")
   res.header("Access-Control-Allow-Credentials", "true")
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
   next()
 })
 
