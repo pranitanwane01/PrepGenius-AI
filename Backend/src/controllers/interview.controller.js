bbb59b001fe8b0ab75ae70c0d13c8a279995b5d5
PRANIT ANWANE <pranitanwane@PRANITs-MacBook-Air.local>
2026-04-09 10:43:58 -0700

    correct some code

--- Backend/src/controllers/interview.controller.js
+++ Backend/src/controllers/interview.controller.js
@@ -8,20 +8,48 @@ const interviewReportModel = require("../models/interviewReport.model")
 /**
  * @description Controller to generate interview report based on user self description, resume and job description.
  */
+// async function generateInterViewReportController(req, res) {
+
+//     const resumeContent = await (new pdfParse.PDFParse(Uint8Array.from(req.file.buffer))).getText()
+//     const { selfDescription, jobDescription } = req.body
+
+//     const interViewReportByAi = await generateInterviewReport({
+//         resume: resumeContent.text,
+//         selfDescription,
+//         jobDescription
+//     })
+
+//     const interviewReport = await interviewReportModel.create({
+//         user: req.user.id,
+//         resume: resumeContent.text,
+//         selfDescription,
+//         jobDescription,
+//         ...interViewReportByAi
+//     })
+
+//     res.status(201).json({
+//         message: "Interview report generated successfully.",
+//         interviewReport
+//     })
+
+// }
+
 async function generateInterViewReportController(req, res) {
 
-    const resumeContent = await (new pdfParse.PDFParse(Uint8Array.from(req.file.buffer))).getText()
+    const resumeData = await pdfParse(req.file.buffer)
+    const resumeContent = resumeData.text
+
     const { selfDescription, jobDescription } = req.body
 
     const interViewReportByAi = await generateInterviewReport({
-        resume: resumeContent.text,
+        resume: resumeContent,
         selfDescription,
         jobDescription
     })
 
     const interviewReport = await interviewReportModel.create({
         user: req.user.id,
-        resume: resumeContent.text,
+        resume: resumeContent,
         selfDescription,
         jobDescription,
         ...interViewReportByAi
@@ -31,7 +59,6 @@ async function generateInterViewReportController(req, res) {
         message: "Interview report generated successfully.",
         interviewReport
     })
-
 }
 
 /**
